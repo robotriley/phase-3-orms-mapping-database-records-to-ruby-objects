ba6e8ebf1f53ab0f040b8796bae4fb099ed0a9e4
riley soloner <rileysoloner@gmail.com>
2022-08-15 11:48:41 -0400

    commit tree 7993cfa623876924230f5a16ab778e47572c1ed6

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,41 @@ class Song
     song.save
   end
 
+ def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  sql = <<-SQL
+  SELECT *
+  FROM songs
+SQL
+
+DB = { conn: SQLite3::Database.new("db/music.db") }
+
+def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  self.all
+
 end
